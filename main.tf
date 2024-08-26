1dc87d86979be503b11659276069a9990898a6b0
jelcano <jelcano@jelcanos-MacBook-Air.local>
2024-08-26 18:59:59 +0200

    deployment fixed, checking ssh problem

--- main.tf
+++ main.tf
@@ -6,8 +6,8 @@ resource "proxmox_vm_qemu" "your-vm" {
     
     # VM General Settings
     target_node = "vm"
-    vmid = "105"
-    name = "tf-node-1"
+    vmid = "106"
+    name = "tf-node-2"
     desc = "First deployment"
 
     # VM Advanced General Settings
@@ -16,6 +16,7 @@ resource "proxmox_vm_qemu" "your-vm" {
     # VM OS Settings
     clone = "ubuntu-template"
 
+    full_clone = true
     # VM System Settings
     agent = 1
     
@@ -33,14 +34,26 @@ resource "proxmox_vm_qemu" "your-vm" {
         model  = "virtio"
     }
 
+    disks {
+        scsi {
+            scsi0 {
+            disk {
+                size = "20G"
+                storage = "local-lvm"
+            }
+            }
+        }
+    }
+
     # VM Cloud-Init Settings
     os_type = "cloud-init"
 
+    #boot = "scsi0;net0"
     # (Optional) IP Address and Gateway
     # ipconfig0 = "ip=0.0.0.0/0,gw=0.0.0.0"
     ipconfig0 = "ip=192.168.50.230/24,gw=192.168.50.1"
     # (Optional) Default User
-    ciuser = "ubuntu"
+    ciuser = "root"
     
     sshkeys = <<EOF
     ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDLLb/a9GAVPcUPVFSqt3+deTCdVcZU/umxZKFtrvbP/FCjmTEDWRIEe6q90wlyHTaRhAjCsGtBs1sVLm9qBh+E1v9NGW/JqBKPgezB3a7K/jrv/Wzli5Pxeg4vPuhG9BdPBCy1pOoO3MnzLai4IRTBgsOR6GSMrs/oi0YXoCRMxdYPyzzSfRUGMjmTJi4SAPX51OJhaI/AooKVulnmvBAspDuectZVvqYg886DkRPecUfeLg0v30s//bfcN4qu27Bffgw+KBIs2uTAnm3Su5BTeuCcA4te4D+1jw+Eq0VV4MrwgPMAKcFugWlMi7wu3Y1ySDtc67t0fDQue6l0XQlqek601W9JZIn7pyeX2Qp1Jee4QTIiJzuhOgwdQeuZvv8D3kbIN+B6Md3Kb64pl/Za3fPhez10W14m0MpNgMwgUMjtZaQjMQ0NrAUazUwYiTA1c/z1iDwEn63virkADyYp4T6K9YefMt2ysl+3kvLor8n39mDuFSDTt4c8at2wycA9JM3PNkLRuJAdabddOC60B3tfnfe8jLw+/IWW7/YmgNjPY68pbICJqY4DxMeKAqvjX9LeJ2CrCWw3lkoJ9x2e2TWUPELf/Q2YlmkxaL9eI3sQjwH4cMrkEZOBbZ5zVXfGo/1600MR5yN7j/iqREgaL3vx3j1sSE7KyRrUf613hw== jelcano@jelcanos-MacBook-Air.local
